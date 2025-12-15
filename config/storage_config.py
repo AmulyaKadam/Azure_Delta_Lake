c7c32d6aec70274d64a964eef9d401c4fa8c363c
Amulya kadam <kadamamulya017@gmail.com>
2025-12-15 19:07:45 +0530

    Add files via upload

---
+++ config/storage_config.py
@@ -0,0 +1,31 @@
+
+def configure_adls(spark, dbutils):
+    """
+    Configure spark.conf for ADLS OAuth access using dbutils.secrets.
+    Assumes a Databricks secret scope named 'my_scope' with keys:
+      - adls_client_id
+      - adls_client_secret
+      - adls_tenant_id
+    Replace scope/key names and account name as necessary.
+    """
+    adls_account_name = "awstorageamulya"  
+
+    
+    client_id = dbutils.secrets.get(scope="my_scope", key="adls-client-id")
+    client_secret = dbutils.secrets.get(scope="my_scope", key="adls-client-secret")
+    tenant_id = dbutils.secrets.get(scope="my_scope", key="adls-tenant-id")
+
+    configs = {
+        f"fs.azure.account.auth.type.{adls_account_name}.dfs.core.windows.net": "OAuth",
+        f"fs.azure.account.oauth.provider.type.{adls_account_name}.dfs.core.windows.net":
+            "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+        f"fs.azure.account.oauth2.client.id.{adls_account_name}.dfs.core.windows.net": client_id,
+        f"fs.azure.account.oauth2.client.secret.{adls_account_name}.dfs.core.windows.net": client_secret,
+        f"fs.azure.account.oauth2.client.endpoint.{adls_account_name}.dfs.core.windows.net":
+            f"https://login.microsoftonline.com/{tenant_id}/oauth2/token"
+    }
+
+    for k, v in configs.items():
+        spark.conf.set(k, v)
+
+
